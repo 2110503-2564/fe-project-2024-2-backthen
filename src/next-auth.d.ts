bd12b37586bef0752ee33f7d0954f94b15e49191
TonJ-TTT <tachatat1402@gmail.com>
2025-03-23 11:01:54 +0700

    fix signin but banner welcom token

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -3,10 +3,7 @@ import NextAuth from 'next-auth'
 declare module "next-auth" {
     interface Session {
         user: {
-            _id: string,
-            name: string,
-            email: string,
-            role: string,
+            success: boolean
             token: string
         }
     }
