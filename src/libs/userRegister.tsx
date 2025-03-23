bdd47e5112c8acebb2ebd882ad3ced3db4f7daa2
TonJ-TTT <tachatat1402@gmail.com>
2025-03-23 20:13:32 +0700

    add headers

--- src/libs/userRegister.tsx
+++ src/libs/userRegister.tsx
@@ -3,7 +3,10 @@ export default async function userRegister(userName:string, userEmail: string, u
     const response = await fetch("https://campground-backend-cyan.vercel.app/api/v1/auth/register", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
         },
         body: JSON.stringify({
             name: userName,
